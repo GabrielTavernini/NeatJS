a3024e299d1fdf311dc2ec94a0534dcf2908c44d
Gabriel <1707gabri@gmail.com>
2019-03-20 11:07:52 +0100

    Advanced Classification Example

--- Examples/Classification/sketch.js
+++ Examples/Classification/sketch.js
@@ -2,31 +2,31 @@ var population;
 var cycles;
 var points = [];
 var myFunction = function(x) { 
-	return height- x+ 150;
+	return Math.sin(x)*0.4 + 0.5;
 };
 
 function setup() {
-	let canvas = createCanvas(400, 400);
+	let canvas = createCanvas(600, 400);
 	canvas.parent('canvascontainer');
 	background(61);
 
 	//Initialize the population
-	population = new Population(50);
+	population = new Population(500);
 	
 	//Generate and Draw points
-	for(let i = 0; i < 200; i++) {
-		let y = Math.random(), x = Math.random();
-		let type = y * height > myFunction(x * width) ? 1 : 0;
+	for(let i = 0; i < 1000; i++) {
+		let y = Math.random(), x = Math.random() * Math.PI * 4;
+		let type = y > myFunction(x) ? 1 : 0;
 		points.push({x: x, y: y, type: type});
 
 		fill(255);
-		ellipse(points[i].x * width, points[i].y * height, 10)
+		ellipse(map(points[i].x, 0, Math.PI*4, 0, width), points[i].y * height, 10)
 	}
 
 	//Draw separation line
-	for(let i = 0; i < width; i += 10) {
+	for(let i = 0; i < width; i += 1) {
 		fill(50);
-		ellipse(i, myFunction(i), 10)
+		ellipse(i, myFunction(map(i, 0, width, 0, Math.PI*4)) * height, 10)
 	}
 }
 
